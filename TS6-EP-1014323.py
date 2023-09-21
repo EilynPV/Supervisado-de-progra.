a227fc9f0287174a34e744e5277f5b5fbbe27e1b
EilynPV <143530990+EilynPV@users.noreply.github.com>
2023-09-21 08:13:41 -0600

    TS6

---
+++ TS6-EP-1014323.py
@@ -0,0 +1,12 @@
+#Introducción a la programación, sección:09"
+#21/09/2023
+#Eilyn Parada 
+#Practicar el uso de las instrucciones print e input en Python
+
+print ("Mi segundo programa")
+Nombre=input("ingrese su nombre")
+Edad=input("ingrese su edad")
+Carrera=input("ingrese su carrera")
+Carné=input("ingrese su carne")
+print("Soy "+ Nombre +", tengo "+ Edad+ " años y estudio la carrera de " +Carrera + ".")
+print("Mi número de carné es, " +Carné+"." )
